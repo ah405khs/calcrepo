774cca0024cfed7ebc0921e7097a3efcf73edd3d
KimHanSol <ah405khs@naver.com>
2016-04-25 10:42:14 +0900

    혼자하는 실습
    아이구야

--- src/main/java/simpleCalculator/Calculator.java
+++ src/main/java/simpleCalculator/Calculator.java
@@ -2,9 +2,9 @@ package simpleCalculator;
 
 public class Calculator {
 	private int result=0;
-	
+
 	public void add(int x, int y) {
-		result=x-y+0; //merge의 의미를 알려주기 위해서.
+		result=x+y+0; //merge의 의미를 알려주기 위해서.
 	}
 	
 	public void sub(int x, int y) {
